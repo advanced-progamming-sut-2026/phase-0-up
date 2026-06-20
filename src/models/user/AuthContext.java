0dfe247b561699ea37ab0854553abe509f5d4cd9
parsa ayati <parsa_ay@yahoo.com>
2026-06-20 14:13:02 +0330

    updated exit menu command and added AuthContext class

---
+++ src/models/user/AuthContext.java
@@ -0,0 +1,17 @@
+package models.user;
+
+public class AuthContext {
+    private User currentUser;
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public void setCurrentUser(User currentUser) {
+        this.currentUser = currentUser;
+    }
+
+    public boolean isLoggedIn() {
+        return currentUser != null;
+    }
+}
