49e19c3062df93513253499af69570a7a509d0c5
amirlari <amirlari131@gmail.com>
2026-06-20 15:42:48 +0330

    collection commands added

--- src/controllers/commands/collection/ShowEntityDetailsCommand.java
+++ src/controllers/commands/collection/ShowEntityDetailsCommand.java
@@ -1,16 +1,35 @@
 package controllers.commands.collection;
 
 import controllers.commands.Command;
+import models.user.Profile;
 import models.user.User;
 import utils.registry.PlantRegistry;
 import utils.registry.ZombieRegistry;
+import views.renderers.MenuRenderer.CollectionMenuRenderer;
 
 public class ShowEntityDetailsCommand implements Command {
     private User currentUser;
     private ZombieRegistry zombieRegistry;
     private PlantRegistry plantRegistry;
+    private ShowListType type;
     private String entityName;
 
+    public ShowEntityDetailsCommand(ShowListType type, String entityName, User currentUser) {
+        this.type = type;
+        this.entityName = entityName;
+        this.currentUser = currentUser;
+        this.plantRegistry = PlantRegistry.getInstance();
+        this.zombieRegistry = ZombieRegistry.getInstance();
+    }
+
     @Override
-    public void execute() {}
+    public void execute() {
+        Profile profile = currentUser.getProfile();
+        CollectionMenuRenderer renderer = new CollectionMenuRenderer();
+        if(type == ShowListType.ZOMBIES || type == ShowListType.ALL_ZOMBIES){
+            renderer.renderZombieDetails(profile, zombieRegistry, entityName);
+        }else {
+            renderer.renderPlantDetails(profile, plantRegistry, entityName);
+        }
+    }
 }
--- src/controllers/commands/collection/UpgradePlantCommand.java
+++ src/controllers/commands/collection/UpgradePlantCommand.java
@@ -1,11 +1,22 @@
 package controllers.commands.collection;
 
 import controllers.commands.Command;
+import controllers.systems.CollectionSystem;
+import models.user.Profile;
 import models.user.User;
 
 public class UpgradePlantCommand implements Command {
     private String plantName;
     private User currentUser;
+
+    public UpgradePlantCommand(String plantName, User currentUser) {
+        this.plantName = plantName;
+        this.currentUser = currentUser;
+    }
+
     @Override
-    public void execute() {}
+    public void execute() {
+        Profile profile = currentUser.getProfile();
+        CollectionSystem.getInstance().upgradePlant(profile, plantName);
+    }
 }
