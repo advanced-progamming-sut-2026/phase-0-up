bcb0976cf5ab97efd68050ee4933d50b25cc3475
amirlari <amirlari131@gmail.com>
2026-06-25 14:00:56 +0330

    SeedSelectionRegex added

---
+++ src/utils/regex/SeedSelectionRegex.java
@@ -0,0 +1,22 @@
+package utils.regex;
+
+public enum SeedSelectionRegex implements Regex{
+    SHOW_ALL_PLANTS("^\\s*show\\s+all\\s+plants\\s*$"),
+    SHOW_AVAILABLE_PLANTS("^\\s*show\\s+availabe\\s+plants\\s*$"),
+    ADD_PLANT("^\\s*add\\s+plant\\s+-t\\s+(?<type>.+?)\\s*$"),
+    REMOVE_PLANT("^\\s*remove\\s+plant\\s+-t\\s+(?<type>.+?)\\s*$"),
+    BOOST_PLANT("^\\s*boost\\s+plant\\s+-t\\s+(?<type>.+?)\\s*$"),
+    START_GAME("^\\s*start\\s+game\\s*$");
+
+
+    private final String pattern;
+
+    SeedSelectionRegex(String pattern) {
+        this.pattern = pattern;
+    }
+
+    @Override
+    public String getPattern() {
+        return pattern;
+    }
+}
