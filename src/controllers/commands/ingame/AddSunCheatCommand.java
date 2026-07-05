e1c95eb09249e4469a4face575a574d3ec223655
amirlari <amirlari131@gmail.com>
2026-07-05 14:41:51 +0330

    added inGame commands about Sun

--- src/controllers/commands/ingame/AddSunCheatCommand.java
+++ src/controllers/commands/ingame/AddSunCheatCommand.java
@@ -2,11 +2,28 @@ package controllers.commands.ingame;
 
 import controllers.commands.Command;
 import models.game.GameSession;
+import utils.Result;
+import views.renderers.InGameRenderer;
 
 public class AddSunCheatCommand implements Command {
     private GameSession gameSession;
+    private final InGameRenderer renderer;
     private int count;
 
+    public AddSunCheatCommand(GameSession gameSession, InGameRenderer renderer, int count) {
+        this.gameSession = gameSession;
+        this.renderer = renderer;
+        this.count = count;
+    }
+
     @Override
-    public void execute() {}
+    public void execute() {
+        if(count <= 0){
+            renderer.render(new Result(false , "Sun count must be positive."));
+            return;
+        }
+        gameSession.increaseSunAmount(count);
+        renderer.render(new Result(true ,
+                "Added " + count + " suns. Current amount: " + gameSession.getSunAmount()));
+    }
 }
--- src/controllers/commands/ingame/CollectSunCommand.java
+++ src/controllers/commands/ingame/CollectSunCommand.java
@@ -1,13 +1,54 @@
 package controllers.commands.ingame;
 
 import controllers.commands.Command;
+import controllers.systems.game.QuestSystem;
+import controllers.systems.game.SunSystem;
+import models.entities.collectibles.SunType;
 import models.game.GameSession;
+import utils.Constants;
+import utils.Result;
+import views.renderers.InGameRenderer;
 
 public class CollectSunCommand implements Command {
     private GameSession gameSession;
+    private SunSystem sunSystem;
+    private InGameRenderer renderer;
+    private final QuestSystem questSystem;
     private int x;
     private int y;
 
+    public CollectSunCommand(GameSession gameSession, SunSystem sunSystem, InGameRenderer renderer,QuestSystem questSystem, int x, int y) {
+        this.gameSession = gameSession;
+        this.sunSystem = sunSystem;
+        this.renderer = renderer;
+        this.x = x;
+        this.y = y;
+        this.questSystem = questSystem;
+    }
+
+
     @Override
-    public void execute() {}
+    public void execute() {
+        if(!isValidCoordinate(x,y)){
+            renderer.render(new Result(false , "Invalid coordinates (" + x + ", " + y + ")."));
+            return;
+        }
+
+        int SunBefore = gameSession.getSunAmount();
+        boolean collected = sunSystem.collectSun(gameSession , x , y);
+        if(!collected){
+            renderer.render(new Result(false , "No sun to collect at (" + x + ", " + y + ")."));
+            return;
+        }
+
+        int gained = gameSession.getSunAmount() - SunBefore;
+        if(questSystem != null){
+            questSystem.recordSunCollected(gained);
+        }
+        renderer.render(new Result(true , "Collected " + gained + " sun."));
+    }
+
+    private boolean isValidCoordinate(int x, int y) {
+        return x >= 0 && x < Constants.BOARD_COLS && y >= 0 && y < Constants.BOARD_ROWS;
+    }
 }
